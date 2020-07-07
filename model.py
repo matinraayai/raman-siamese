87cb155d49fc9b991bf246ed5ea74cd7716a199a
matinraayai <raayaiardani.m@northeastern.edu>
2020-07-07 00:13:17 -0400

    Added new Siamese DeepCID model.

--- model.py
+++ model.py
@@ -3,43 +3,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Siamese(nn.Module):
+def _init_weight(module: nn.Module):
+    return
 
-    def __init__(self):
-        super(Siamese, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(1, 64, 10),  # 64@96*96
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  # 64@48*48
-            nn.Conv2d(64, 128, 7),
-            nn.ReLU(),    # 128@42*42
-            nn.MaxPool2d(2),   # 128@21*21
-            nn.Conv2d(128, 128, 4),
-            nn.ReLU(), # 128@18*18
-            nn.MaxPool2d(2), # 128@9*9
-            nn.Conv2d(128, 256, 4),
-            nn.ReLU(),   # 256@6*6
-        )
-        self.liner = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
+
+class Siamese1D(nn.Module):
+    def __init__(self, dropout_rate: float):
+        super(Siamese1D, self).__init__()
+        self.network = nn.Sequential(nn.Conv1d(1, 32,
+                                               kernel_size=5,
+                                               stride=2,
+                                               padding_mode='zeros'),
+                                     nn.ReLU(inplace=True),
+                                     nn.MaxPool1d(2),
+                                     nn.Dropout(dropout_rate),
+                                     nn.Conv1d(32, 64,
+                                               kernel_size=5,
+                                               stride=2,
+                                               padding_mode='zeros'),
+                                     nn.ReLU(inplace=True),
+                                     nn.MaxPool1d(2),
+                                     nn.Dropout(dropout_rate),
+                                     nn.Flatten(),
+                                     nn.Linear(3456, 4096),
+                                     nn.Sigmoid()
+                                     )
         self.out = nn.Linear(4096, 1)
 
-    def forward_one(self, x):
-        x = self.conv(x)
-        x = x.view(x.size()[0], -1)
-        x = self.liner(x)
-        return x
-
     def forward(self, x1, x2):
-        out1 = self.forward_one(x1)
-        out2 = self.forward_one(x2)
+        out1 = self.network(x1)
+        out2 = self.network(x2)
         dis = torch.abs(out1 - out2)
-        out = self.out(dis)
-        #  return self.sigmoid(out)
-        return out
-
-
-# for test
-if __name__ == '__main__':
-    net = Siamese()
-    print(net)
-    print(list(net.parameters()))
+        return self.out(dis)
