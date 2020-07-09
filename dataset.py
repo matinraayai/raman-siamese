2f13d5084bc877b630fcb22de5716a272b5fdaa5
matinraayai <raayaiardani.m@northeastern.edu>
2020-07-09 17:29:58 -0400

    Added new component-wise dataset.

--- dataset.py
+++ dataset.py
@@ -3,47 +3,22 @@ import numpy as np
 from sklearn import preprocessing
 
 
-class RamanTrain(Dataset):
-    def __init__(self, path, compound, fold):
-        x = np.load(path + "/" + str(compound) + 'component.npy').astype(np.float)
-        y = np.load(path + "/" + str(compound) + 'label.npy').astype(np.float)
+class RamanDataset(Dataset):
+    def __init__(self, path, train_data_fold, train=True):
+        x = np.load(path + '/spectrum.npy').astype(np.float)
+        y = np.load(path + '/components.npy').astype(np.float)
         assert (len(x) == len(y))
-        train_fold_pos = int(len(x) * fold)
+        fold_pos = int(len(x) * train_data_fold)
         # Pre-processing Xs:============================================================================================
-        x = x[:train_fold_pos]
+        x = x[:fold_pos] if train else x[fold_pos:]
         # standard Gaussian scalar:
         scaler = preprocessing.StandardScaler().fit(x)
         self.x = scaler.transform(x)
         # Pre-processing Ys:============================================================================================
-        y_neg = np.ones(y.shape) - y
-        y = np.concatenate((y, y_neg), axis=1)
-        self.y = y[:train_fold_pos]
+        self.y = y[:, :fold_pos] if train else y[:, fold_pos:]
 
     def __getitem__(self, item):
-        return self.x[item], self.y[item]
-
-    def __len__(self):
-        return len(self.x)
-
-
-class RamanTest(Dataset):
-    def __init__(self, path, compound, fold):
-        x = np.load(path + "/" + str(compound) + 'component.npy').astype(np.float)
-        y = np.load(path + "/" + str(compound) + 'label.npy').astype(np.float)
-        assert (len(x) == len(y))
-        test_fold_pos = int(len(x) * fold)
-        # Pre-processing Xs:========================================================================================
-        x = x[test_fold_pos:]
-        # standard Gaussian scalar:
-        scaler = preprocessing.StandardScaler().fit(x)
-        self.x = scaler.transform(x)
-        # Pre-processing Ys:========================================================================================
-        y_neg = np.ones(y.shape) - y
-        y = np.concatenate((y, y_neg), axis=1)
-        self.y = y[test_fold_pos:]
-
-    def __getitem__(self, item):
-        return self.x[item], self.y[item]
+        return self.x[item], self.y[:, item]
 
     def __len__(self):
         return len(self.x)
--- train.py
+++ train.py
@@ -1,18 +1,11 @@
 import torch
 import pickle
-import torchvision
-from torchvision import transforms
-import torchvision.datasets as dset
-from torchvision import transforms
-from mydataset import OmniglotTrain, OmniglotTest
+from dataset import RamanDataset
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
-import matplotlib.pyplot as plt
 from model import Siamese1D
 import time
 import numpy as np
-import gflags
-import sys
 from collections import deque
 import os
 import argparse
@@ -22,10 +15,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser("Siamese network training script.")
     parser.add_argument("--cuda", type=bool, default=True,
                         help="Use CUDA")
-    parser.add_argument("--train_path", type=str, default="images_background",
-                        help="training folder path")
-    parser.add_argument("--test_path", type=str, default="images_evaluation",
-                        help="testing folder path")
+    parser.add_argument("--data_path", type=str, default="data",
+                        help="dataset folder path")
+    parser.add_argument("--train_data_fold", type=float, default=0.7,
+                        help="Amount of original data to be used in training")
     parser.add_argument("--model_path", type=str, default="model/siamese",
                         help="path to save model checkpoints")
     parser.add_argument("--n_way", type=int, default=40,
@@ -45,37 +38,30 @@ if __name__ == '__main__':
                         help="Model testing frequency per number of iterations")
     parser.add_argument("--max_iter", type=int, default=50000,
                         help="Maximum number of iterations used before stopping training")
-    parser.add_argument("--gpu_ids", type=str, default="0",
-                        help="Id of the GPUs used in training")
+    args = parser.parse_args()
 
-    data_transforms = transforms.Compose([
-        transforms.RandomAffine(15),
-        transforms.ToTensor()
-    ])
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_ids
+    print("use gpu:", args.gpu_ids, "to train.")
 
+    train_data = RamanDataset(args.data_path, args.train_data_fold)
+    test_data = RamanDataset(args.data_path, args.train_data_fold, train=False)
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = Flags.gpu_ids
-    print("use gpu:", Flags.gpu_ids, "to train.")
+    trainLoader = DataLoader(train_data, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+    testLoader = DataLoader(test_data, batch_size=args.n_way, shuffle=False, num_workers=args.num_workers)
 
-    trainSet = OmniglotTrain(Flags.train_path, transform=data_transforms)
-    testSet = OmniglotTest(Flags.test_path, transform=transforms.ToTensor(), times = Flags.times, way = Flags.way)
-    testLoader = DataLoader(testSet, batch_size=Flags.way, shuffle=False, num_workers=Flags.workers)
-
-    trainLoader = DataLoader(trainSet, batch_size=Flags.batch_size, shuffle=False, num_workers=Flags.workers)
-
-    loss_fn = torch.nn.BCEWithLogitsLoss(size_average=True)
-    net = Siamese1D()
+    loss_fn = torch.nn.BCEWithLogitsLoss()
+    net = Siamese1D(0.2)
 
     # multi gpu
-    if len(Flags.gpu_ids.split(",")) > 1:
+    if len(args.gpu_ids.split(",")) > 1:
         net = torch.nn.DataParallel(net)
 
-    if Flags.cuda:
+    if args.cuda:
         net.cuda()
 
     net.train()
 
-    optimizer = torch.optim.Adam(net.parameters(),lr = Flags.lr )
+    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
     optimizer.zero_grad()
 
     train_loss = []
@@ -84,9 +70,9 @@ if __name__ == '__main__':
     queue = deque(maxlen=20)
 
     for batch_id, (img1, img2, label) in enumerate(trainLoader, 1):
-        if batch_id > Flags.max_iter:
+        if batch_id > args.max_iter:
             break
-        if Flags.cuda:
+        if args.cuda:
             img1, img2, label = Variable(img1.cuda()), Variable(img2.cuda()), Variable(label.cuda())
         else:
             img1, img2, label = Variable(img1), Variable(img2), Variable(label)
@@ -96,29 +82,28 @@ if __name__ == '__main__':
         loss_val += loss.item()
         loss.backward()
         optimizer.step()
-        if batch_id % Flags.show_every == 0 :
+        if batch_id % args.show_every == 0 :
             print('[%d]\tloss:\t%.5f\ttime lapsed:\t%.2f s'%(batch_id, loss_val/Flags.show_every, time.time() - time_start))
             loss_val = 0
             time_start = time.time()
-        if batch_id % Flags.save_every == 0:
+        if batch_id % args.save_every == 0:
             torch.save(net.state_dict(), Flags.model_path + '/model-inter-' + str(batch_id+1) + ".pt")
         if batch_id % Flags.test_every == 0:
             right, error = 0, 0
             for _, (test1, test2) in enumerate(testLoader, 1):
-                if Flags.cuda:
+                if args.cuda:
                     test1, test2 = test1.cuda(), test2.cuda()
-                test1, test2 = Variable(test1), Variable(test2)
                 output = net.forward(test1, test2).data.cpu().numpy()
                 pred = np.argmax(output)
                 if pred == 0:
                     right += 1
-                else: error += 1
+                else:
+                    error += 1
             print('*'*70)
             print('[%d]\tTest set\tcorrect:\t%d\terror:\t%d\tprecision:\t%f'%(batch_id, right, error, right*1.0/(right+error)))
             print('*'*70)
-            queue.append(right*1.0/(right+error))
+            queue.append(right * 1.0 / (right + error))
         train_loss.append(loss_val)
-    #  learning_rate = learning_rate * 0.95
 
     with open('train_loss', 'wb') as f:
         pickle.dump(train_loss, f)
